44b15d33799a18b8651d437986e1b03d269264e6
Christalin Dorsey <christalin.dorsey03@gmail.com>
2024-01-12 19:38:01 -0800

    Added swim animation

--- nyns-2024/Assets/_Scripts/EndAnimation.cs
+++ nyns-2024/Assets/_Scripts/EndAnimation.cs
@@ -7,6 +7,7 @@ public class EndAnimation : MonoBehaviour
     [SerializeField] GameObject _siren;
     [SerializeField] Transform _endPoint;
     [SerializeField] float _swimSpeed;
+    [SerializeField] RuntimeAnimatorController _swimAnim;
 
     private void Update()
     {
@@ -21,6 +22,7 @@ public class EndAnimation : MonoBehaviour
         yield return new WaitForSeconds(1);
         LeanTween.rotate(_siren, new Vector3(75f, 180f, 0f), 3f).setEaseOutCubic();
         yield return new WaitForSeconds(1.5f);
+        _siren.GetComponent<Animator>().runtimeAnimatorController = _swimAnim;
         LeanTween.move(_siren, _endPoint.position, _swimSpeed).setEaseOutCubic();
         this.enabled = false;
     }
